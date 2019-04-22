da95597eda3b29493df56622eca5e0c677f53035
nbigger <nick.bigger@gmail.com>
2019-04-21 21:38:08 -0700

    basic getters

--- pyHue.py
+++ pyHue.py
@@ -1,18 +1,27 @@
 import requests
 from os import environ
 
+def get_lights():
+  newURL = URL + environ['USERNAME'] + "/lights"
+  print get(newURL)
+
+def get_light(Id):
+  newURL = URL + environ['USERNAME'] + "/lights/" + str(Id)
+  print get(newURL)
+
+def get(URL):
+  r = requests.get(url = URL) 
+  return r.json();
+
 def main():
   if environ.get('USERNAME') is not None:
-    URL = "http://10.0.0.10/api/" + environ['USERNAME'] + "/lights"
-      
-    # sending get request and saving the response as response object 
-    r = requests.get(url = URL) 
-      
-    # extracting data in json format 
-    data = r.json()
-    print data
+    # get_lights()
+    get_light(1)
+
   else:
     print "Enviornmental variable USERNAME must be set. Use export USERNAME='your_username' to run."
+
+URL = "http://10.0.0.10/api/"
 
 if __name__ == "__main__":
     main()
