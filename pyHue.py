bdcb9cf1a5b80c8f8c1253526aa59470f1d5e07b
nbigger <nick.bigger@gmail.com>
2019-04-21 21:48:50 -0700

    implemented set light

--- pyHue.py
+++ pyHue.py
@@ -5,9 +5,18 @@ def get_lights():
   newURL = URL + environ['USERNAME'] + "/lights"
   print get(newURL)
 
-def get_light(Id):
-  newURL = URL + environ['USERNAME'] + "/lights/" + str(Id)
+def get_light(id):
+  newURL = URL + environ['USERNAME'] + "/lights/" + str(id)
   print get(newURL)
+
+def set_light(id, state):
+  newURL = URL + environ['USERNAME'] + "/lights/" + str(id) + "/state"
+  data = '{"on": ' + state + "}"
+
+  r = requests.put(newURL, data = data) 
+    
+  response = r.text 
+  print response
 
 def get(URL):
   r = requests.get(url = URL) 
@@ -16,7 +25,8 @@ def get(URL):
 def main():
   if environ.get('USERNAME') is not None:
     # get_lights()
-    get_light(1)
+    # get_light(1)
+    set_light(1, "true")
 
   else:
     print "Enviornmental variable USERNAME must be set. Use export USERNAME='your_username' to run."
