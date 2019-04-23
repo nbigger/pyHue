3489c577a3fad043c0af6e107534792ed85cc4c2
nbigger <nick.bigger@gmail.com>
2019-04-22 21:09:53 -0700

    added authentication for new device without username

--- pyHue.py
+++ pyHue.py
@@ -2,29 +2,47 @@ import requests as req
 
 class Bridge(object):
 
-  def __init__(self, ip, username):
+  def __init__(self, ip, username=None):
     self.ip = ip
-    self.url = "http://"+ip+"/api/"+username
-    print ("new bridge at ip " + ip)
+    self.url = "http://"+ip+"/api"
+    self.username = username
+
+    if self.username is None:
+      self.authenticate()
+
+    else:
+      print ("Success. New bridge found at " + ip)
 
   def get_lights(self):
     path = "/lights"
     return self.request('GET', path, None)
 
   def get_light(self, id):
-    path = environ['USERNAME'] + "/lights/" + str(id)
+    path = "/lights/" + str(id)
     return self.request('GET', path, None)
 
   def set_light(self, id, state):
     path = "/lights/" + str(id) + "/state"
     data = '{"on": ' + state + "}"
 
-    return self.request('PUT', path, data = data) 
-      
-    # response = r.text 
-    # return response
+    return self.request('PUT', path, data = data)
+
+  def authenticate(self):
+    data = '{"devicetype": "pyHue_user"}'
+    response = self.request('POST', '', data)
+
+    if 'error' in response[0]:
+      description = response[0]['error']['description']
+      raise Exception(description)
+
+    elif 'success' in response[0]:
+      self.username = response[0]['success']['username']
 
   def request(self, method, path, data):
-    URL = self.url + path
+    if self.username is not None:
+      URL = self.url + self.username + path
+    else:
+      URL = self.url
+
     r = req.request(method=method, url=URL, data=data) 
     return r.json();
--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from pyHue import Bridge
 from os import environ
 
 if environ.get('USERNAME') is not None:
-  bridge = Bridge(ip='10.0.0.10', username=environ['USERNAME'])
+  bridge = Bridge(ip='10.0.0.10', username='cblkV1H24kZy6g3VniKRK4-78mx3I4T7dJZHFXVZ')
 
   # get_lights()
   # get_light(1)
   bridge.set_light(1, "true")
 
 else:
-  print ("Enviornmental variable USERNAME must be set. Use export USERNAME='your_username' to run.")
+  bridge = Bridge(ip='10.0.0.10')
